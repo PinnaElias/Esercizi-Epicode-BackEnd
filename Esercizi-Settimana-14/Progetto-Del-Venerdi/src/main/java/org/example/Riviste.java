92d4f1034927b80faa6f8c733d9354a443751069
eliaspinna <eliaspinna99@gmail.com>
2024-04-26 19:04:55 +0200

    .

--- Esercizi-Settimana-14/Progetto-Del-Venerdi/src/main/java/org/example/Riviste.java
+++ Esercizi-Settimana-14/Progetto-Del-Venerdi/src/main/java/org/example/Riviste.java
@@ -3,15 +3,31 @@ package org.example;
 import java.sql.Array;
 
 public class Riviste extends Stampato {
-    public Riviste(String ISBN, String title, Long year, Long lenght) {
+    public Riviste(String ISBN, String title, Long year, Long lenght, String selectedPeriodicity) {
         super(ISBN, title, year, lenght);
     }
 
-    String[] periodicity = {"SETTIMANALE", "MENSILE", "SEMESTRALE"};
+    protected String[] periodicity = {"SETTIMANALE", "MENSILE", "SEMESTRALE"};
+    protected String selectedPeriodicity;
 
     public String[] getPeriodicity() {
         return periodicity;
     }
 
+    public String getSelectedPeriodicity() {
+        return selectedPeriodicity;
+    }
+
     //fai il setter appropriato
+
+    public void setPeriodicity(String Periodicity) {
+        for (String prd : periodicity){
+            if (prd.equals(Periodicity)) {
+                this.selectedPeriodicity = Periodicity;
+                return;
+            }  else {
+                System.out.println("Invalid input");
+            }
+        }
+    }
 }
