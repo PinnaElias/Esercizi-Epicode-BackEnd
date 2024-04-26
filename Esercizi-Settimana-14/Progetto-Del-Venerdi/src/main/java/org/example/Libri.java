d381111ad8a276e33ce7d765b3ed213fc3fc15d1
eliaspinna <eliaspinna99@gmail.com>
2024-04-26 18:46:48 +0200

    .

--- Esercizi-Settimana-14/Progetto-Del-Venerdi/src/main/java/org/example/Libri.java
+++ Esercizi-Settimana-14/Progetto-Del-Venerdi/src/main/java/org/example/Libri.java
@@ -2,4 +2,7 @@ package org.example;
 
 public class Libri extends Stampato {
 
+    public Libri(String ISBN, String title, Long year, Long lenght) {
+        super(ISBN, title, year, lenght);
+    }
 }
