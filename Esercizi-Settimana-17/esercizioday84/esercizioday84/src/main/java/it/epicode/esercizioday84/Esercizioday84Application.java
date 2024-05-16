fd402d8859eca80b620435e825e063c72fd2d6c3
eliaspinna <eliaspinna99@gmail.com>
2024-05-16 16:14:21 +0200

    unversioned files

---
+++ Esercizi-Settimana-17/esercizioday84/esercizioday84/src/main/java/it/epicode/esercizioday84/Esercizioday84Application.java
@@ -0,0 +1,15 @@
+package it.epicode.esercizioday84;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizioday84Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Esercizioday84Application.class, args);
+
+	}
+
+}
---
+++ Esercizi-Settimana-17/esercizioday84/esercizioday84/src/test/java/it/epicode/esercizioday84/Esercizioday84ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.esercizioday84;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esercizioday84ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
