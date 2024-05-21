36d833ae46d1836523b9ce29a85a0a792850cafb
eliaspinna <eliaspinna99@gmail.com>
2024-05-21 16:48:52 +0200

    Esercizio 21/05

--- Esercizi-Settimana-18/esercizioDay86/esercizioDay86/src/main/java/it/epicode/esercizioDay86/controller/PostController.java
+++ Esercizi-Settimana-18/esercizioDay86/esercizioDay86/src/main/java/it/epicode/esercizioDay86/controller/PostController.java
@@ -15,32 +15,37 @@ import java.util.List;
 @RequestMapping("/posts")
 public class PostController {
 
-//  primo passo: collegare al service
+    //  primo passo: collegare al service
     @Autowired
     private PostService postService;
 
-//nei contesti reali si usa la responseEntity
+    //nei contesti reali si usa la responseEntity
     @GetMapping("/") //mappa il metodo HTTP, in questo caso GET
-    public ResponseEntity<List<Post>> getPosts(){
+    public ResponseEntity<List<Post>> getPosts() {
         //una ResponseEntity è un entità che come risposta (personalizzata) ci torna <List><Post>>
         //in questo caso la usiamo per ottenere tutti i post e un codice di risposta 200
         return new ResponseEntity<>(postService.getAllPosts(), HttpStatusCode.valueOf(200));
-
-//        BTW, per avere lo status "ok" si può anche usare il seguente metodo:
-//        return new ResponseEntity<>(postService.getAllPosts(), HttpStatus.OK);
+//       BTW, per avere lo status "ok" si può anche usare il seguente metodo:
+//       return new ResponseEntity<>(postService.getAllPosts(), HttpStatus.OK);
     }
 
 //  a differenza di prima ci facciamo tornare un JSON con un singolo post
     @GetMapping("/{id}")
-    public ResponseEntity<Post> getSinglePost(@PathVariable(name = "id") int id){
-        return new ResponseEntity<>(postService.getPostById(id), HttpStatus.OK);
+    public ResponseEntity<Post> getSinglePost(@PathVariable int id) {
+        Post post = postService.getPostById(id);
+        if (post == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(post, HttpStatus.OK);
     }
 
     @PostMapping("/")
-    public ResponseEntity<Post> createSinglePost(@RequestBody CreatePostRequestBody newPost){
+    public ResponseEntity<Post> createSinglePost(@RequestBody CreatePostRequestBody newPost) {
         return new ResponseEntity<>(postService.addPost(newPost), HttpStatusCode.valueOf(201));
 
 //        BTW, per avere lo status "CREATED" si può anche usare il seguente metodo:
 //        return new ResponseEntity<>(postService.getAllPosts(), HttpStatus.CREATED);
     }
+
+
 }
--- Esercizi-Settimana-18/esercizioDay86/esercizioDay86/src/main/java/it/epicode/esercizioDay86/models/CreatePostRequestBody.java
+++ Esercizi-Settimana-18/esercizioDay86/esercizioDay86/src/main/java/it/epicode/esercizioDay86/models/CreatePostRequestBody.java
@@ -1,16 +1,28 @@
 package it.epicode.esercizioDay86.models;
 
 import lombok.Data;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 @Data
 public class CreatePostRequestBody {
 //  questo NON è il post ma il modello da rispettare per poter creare il post.
 //  per questo non necessita di un costruttore personalizzato o di un id specifico,
 //  è il corrispettivo di un interfaccia di typeScript
+
+    @NonNull
     private String categoria;
+
+    @NonNull
     private String titolo;
+
+    @NonNull
     private String cover;
+
+    @NonNull
     private String contenuto;
+
+    @NonNull
     private int tempoDiLettura;
 
 }
