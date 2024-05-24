e80deabd4aeb112d9e1fe91561c7b22eedf64ca0
eliaspinna <eliaspinna99@gmail.com>
2024-05-24 10:46:07 +0200

    .

---
+++ Esercizi-Settimana-18/progettofinesettimana/progettofinesettimana/src/main/java/it/epicode/progettofinesettimana/ProgettofinesettimanaApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.progettofinesettimana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettofinesettimanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettofinesettimanaApplication.class, args);
+	}
+
+}
