6a2976cf474e2d6cd54d53320fcfcb31fe22ff26
eliaspinna <eliaspinna99@gmail.com>
2024-06-01 12:31:37 +0200

    ri-creazione progetto

---
+++ Esercizi-Settimana-19/progettoweekend/progettoweekend/src/main/java/it/epicode/progettoweekend/ProgettoweekendApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.progettoweekend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoweekendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoweekendApplication.class, args);
+	}
+
+}
